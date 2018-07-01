9159708e188fc5f0e9d2038aefdf737f7ce5c68e
Christian Ziegler <christian@acolono.com>
2018-07-01 14:23:33 +0200

    Install facebook driver.

--- hoaxlybot/.discovery/discovery_asset_types.php
+++ hoaxlybot/.discovery/discovery_asset_types.php
@@ -4,6 +4,16 @@ return array (
   array (
     0 => 
     array (
+      'value' => 'stubs/facebook.php',
+      'package' => 'botman/driver-facebook',
+      'packageDir' => 'vendor/botman/driver-facebook/',
+      'priority' => 0.0,
+      'metadata' => 
+      array (
+      ),
+    ),
+    1 => 
+    array (
       'value' => 'stubs/slack.php',
       'package' => 'botman/driver-slack',
       'packageDir' => 'vendor/botman/driver-slack/',
@@ -12,7 +22,7 @@ return array (
       array (
       ),
     ),
-    1 => 
+    2 => 
     array (
       'value' => 'stubs/web.php',
       'package' => 'botman/driver-web',
@@ -27,6 +37,66 @@ return array (
   array (
     0 => 
     array (
+      'value' => 'BotMan\\Drivers\\Facebook\\FacebookDriver',
+      'package' => 'botman/driver-facebook',
+      'packageDir' => 'vendor/botman/driver-facebook/',
+      'priority' => 0.0,
+      'metadata' => 
+      array (
+      ),
+    ),
+    1 => 
+    array (
+      'value' => 'BotMan\\Drivers\\Facebook\\FacebookAudioDriver',
+      'package' => 'botman/driver-facebook',
+      'packageDir' => 'vendor/botman/driver-facebook/',
+      'priority' => 0.0,
+      'metadata' => 
+      array (
+      ),
+    ),
+    2 => 
+    array (
+      'value' => 'BotMan\\Drivers\\Facebook\\FacebookFileDriver',
+      'package' => 'botman/driver-facebook',
+      'packageDir' => 'vendor/botman/driver-facebook/',
+      'priority' => 0.0,
+      'metadata' => 
+      array (
+      ),
+    ),
+    3 => 
+    array (
+      'value' => 'BotMan\\Drivers\\Facebook\\FacebookImageDriver',
+      'package' => 'botman/driver-facebook',
+      'packageDir' => 'vendor/botman/driver-facebook/',
+      'priority' => 0.0,
+      'metadata' => 
+      array (
+      ),
+    ),
+    4 => 
+    array (
+      'value' => 'BotMan\\Drivers\\Facebook\\FacebookLocationDriver',
+      'package' => 'botman/driver-facebook',
+      'packageDir' => 'vendor/botman/driver-facebook/',
+      'priority' => 0.0,
+      'metadata' => 
+      array (
+      ),
+    ),
+    5 => 
+    array (
+      'value' => 'BotMan\\Drivers\\Facebook\\FacebookVideoDriver',
+      'package' => 'botman/driver-facebook',
+      'packageDir' => 'vendor/botman/driver-facebook/',
+      'priority' => 0.0,
+      'metadata' => 
+      array (
+      ),
+    ),
+    6 => 
+    array (
       'value' => 'BotMan\\Drivers\\Slack\\SlackDriver',
       'package' => 'botman/driver-slack',
       'packageDir' => 'vendor/botman/driver-slack/',
@@ -35,7 +105,7 @@ return array (
       array (
       ),
     ),
-    1 => 
+    7 => 
     array (
       'value' => 'BotMan\\Drivers\\Web\\WebDriver',
       'package' => 'botman/driver-web',
@@ -49,6 +119,56 @@ return array (
   'botman/commands' => 
   array (
     0 => 
+    array (
+      'value' => 'BotMan\\Drivers\\Facebook\\Commands\\AddPersistentMenu',
+      'package' => 'botman/driver-facebook',
+      'packageDir' => 'vendor/botman/driver-facebook/',
+      'priority' => 0.0,
+      'metadata' => 
+      array (
+      ),
+    ),
+    1 => 
+    array (
+      'value' => 'BotMan\\Drivers\\Facebook\\Commands\\AddStartButtonPayload',
+      'package' => 'botman/driver-facebook',
+      'packageDir' => 'vendor/botman/driver-facebook/',
+      'priority' => 0.0,
+      'metadata' => 
+      array (
+      ),
+    ),
+    2 => 
+    array (
+      'value' => 'BotMan\\Drivers\\Facebook\\Commands\\AddGreetingText',
+      'package' => 'botman/driver-facebook',
+      'packageDir' => 'vendor/botman/driver-facebook/',
+      'priority' => 0.0,
+      'metadata' => 
+      array (
+      ),
+    ),
+    3 => 
+    array (
+      'value' => 'BotMan\\Drivers\\Facebook\\Commands\\WhitelistDomains',
+      'package' => 'botman/driver-facebook',
+      'packageDir' => 'vendor/botman/driver-facebook/',
+      'priority' => 0.0,
+      'metadata' => 
+      array (
+      ),
+    ),
+    4 => 
+    array (
+      'value' => 'BotMan\\Drivers\\Facebook\\Commands\\Nlp',
+      'package' => 'botman/driver-facebook',
+      'packageDir' => 'vendor/botman/driver-facebook/',
+      'priority' => 0.0,
+      'metadata' => 
+      array (
+      ),
+    ),
+    5 => 
     array (
       'value' => 'BotMan\\Drivers\\Slack\\Commands\\SlackRTMListenCommand',
       'package' => 'botman/driver-slack',
